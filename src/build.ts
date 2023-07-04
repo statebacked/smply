75525ba58b42ff43814ce8eba53e162beb01bac9
Adam Berger <adam@simplystated.dev>
2023-07-04 17:40:58 -0400

    build: build esm for node

--- src/build.ts
+++ src/build.ts
@@ -1,4 +1,4 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.17.19/mod.js";
+import * as esbuild from "https://deno.land/x/esbuild@v0.18.11/mod.js";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts";
 import * as path from "https://deno.land/std@0.192.0/path/mod.ts";
 
