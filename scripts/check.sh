97d5b261c42a9b20d5da8e867a83e736904df41d
Adam Berger <adam@simplystated.dev>
2023-07-04 10:33:47 -0400

    build: add scripts

---
+++ scripts/check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+cmd="deno check ./mod.ts"
+
+if [ "$1" = "--no-docker" ]; then
+  exec ${cmd}
+fi
+
+exec docker run --rm -it -u "${UID}:${UID}" -v "$(pwd)/deno_dir:/home/deno/deno_dir" -v "$(pwd):/home/deno/code:ro" -e "DENO_DIR=/home/deno/deno_dir" -e "HOME=/home/deno" -v "${HOME}/.smply:/home/deno/.smply" --workdir /home/deno/code denoland/deno:ubuntu-1.34.3 ${cmd}
---
+++ scripts/fmt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+cmd="deno fmt ./mod.ts"
+
+if [ "$1" = "--no-docker" ]; then
+  exec ${cmd}
+fi
+
+exec docker run --rm -it -u "${UID}:${UID}" -v "$(pwd)/deno_dir:/home/deno/deno_dir" -v "$(pwd):/home/deno/code" -e "DENO_DIR=/home/deno/deno_dir" -e "HOME=/home/deno" -v "${HOME}/.smply:/home/deno/.smply" --workdir /home/deno/code denoland/deno:ubuntu-1.34.3 ${cmd}
