c58939ef3e0f7b48f9c6c59ed57892a56dc9c884
Adam Berger <adam@simplystated.dev>
2023-07-26 13:08:24 -0400

    build: no need for port

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -8,4 +8,4 @@ function cleanup {
 
 trap cleanup EXIT
 
-exec docker run --rm -it -p 3000:3000 -v "$(pwd):/work" -v "${homedir}:/me" -e "HOME=/me" --user ${UID} --entrypoint=npm --workdir=/work node:16.17-buster-slim "$@"
+exec docker run --rm -it -v "$(pwd):/work" -v "${homedir}:/me" -e "HOME=/me" --user ${UID} --entrypoint=npm --workdir=/work node:16.17-buster-slim "$@"
