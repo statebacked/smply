e02f19d1011e1beea25388ccbd9509d073c61c2d
Adam Berger <adam@simplystated.dev>
2023-08-08 17:56:59 -0400

    build: use modern node

--- scripts/smply.sh
+++ scripts/smply.sh
@@ -8,4 +8,4 @@ function cleanup {
 
 trap cleanup EXIT
 
-exec docker run --rm -it -u "${UID}:${UID}" -v "$(pwd):/work" -v "${homedir}:/me" -e "HOME=/me" -v "${HOME}/.smply:/me/.smply" --workdir /work node:16.17-buster-slim node ./dist/index.js "$@"
+exec docker run --rm -it -u "${UID}:${UID}" -v "$(pwd):/work" -v "${homedir}:/me" -e "HOME=/me" -v "${HOME}/.smply:/me/.smply" --workdir /work node:18.17-buster-slim node ./dist/index.js "$@"
