8e343289e695ac5f5e9d6b88e915b34d7a3ae1f4
Adam Berger <adam@simplystated.dev>
2023-10-09 10:27:29 -0400

    build: better smply script

--- scripts/smply.sh
+++ scripts/smply.sh
@@ -8,4 +8,6 @@ function cleanup {
 
 trap cleanup EXIT
 
-exec docker run --rm -it -u "${UID}:${UID}" -v "$(pwd):/work" -v "${homedir}:/me" -e "HOME=/me" -v "${HOME}/.smply:/me/.smply" --workdir /work node:18.17-buster-slim node ./dist/index.js "$@"
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+exec docker run --rm -it -u "${UID}:${UID}" -v "$(pwd):/work" -v "${script_dir}/..:/smply" -v "${homedir}:/me" -e "HOME=/me" -v "${HOME}/.smply:/me/.smply" --workdir /work node:18.17-buster-slim node "/smply/dist/index.js" "$@"
